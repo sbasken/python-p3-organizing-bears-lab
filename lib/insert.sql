cc20dfdf5843510203d1a5212c582bb20cfaf131
Saki <sbasken@gmail.com>
2023-03-17 13:43:47 -0700

    create, insert, and query bears table

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex) VALUES ("Mr\. Chocolate", 'M');
+INSERT INTO bears (name, sex) VALUES ("Rowdy", 'F');
+INSERT INTO bears (name, sex) VALUES ("Tabitha", 'M');
+INSERT INTO bears (name, sex) VALUES ("Sergeant Brown", 'M');
+INSERT INTO bears (name, sex) VALUES ("Melissa", 'F');
+INSERT INTO bears (name, sex) VALUES ("Grinch", 'F');
+INSERT INTO bears (name, sex) VALUES ("Wendy", 'M');
+INSERT INTO bears (name, sex) VALUES (Null, 'F');
